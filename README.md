43763da48b735da30b82713feb40a4bf8d0febf5
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-03-26 18:56:05 +0300

    step 6, update brain_calc, readme, scripts

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 [![Actions Status](https://github.com/akelaPro/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/akelaPro/python-project-49/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/4966aafa308e3d45a007/maintainability)](https://codeclimate.com/github/akelaPro/python-project-49/maintainability)
 https://asciinema.org/a/ltcrgtHoPVTCpNvCKN8ZJkDIm
+https://asciinema.org/a/8QwCbfWLdx8tgzh7335xUZB2F
---
+++ brain_games/game/calc_code.py
@@ -0,0 +1,33 @@
+from random import randint, choice
+from brain_games.cli import welcome_user
+def calc_game(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+
+
+def calc():
+    name = welcome_user()
+    print("What is the result of the expression?")
+    count = 0
+    while count <3:
+        num1 = randint(1,101)
+        num2 = randint(1,101)
+        operator = choice(['+','-','*'])
+        print(f'Question: {num1} {operator} {num2}')
+        n = int(input('yor answer: '))
+        if calc_game(num1, num2, operator) == n:
+            print('Correct!')
+            count +=1
+        else:
+            print(f"{n} is wrong answer ;(. Correct answer was {calc_game(num1, num2, operator)}.")
+            print(f"let's try again,{name}")
+    if count ==3:
+        print(f'Congratulation, {name}!')
+
+
+if __name__ == '__main__':
+    calc()
