7e44d485dddd58423ded924616c56911dd1c6910
ilya228 <ilya228@ubuntu.myguest.virtualbox.org>
2024-03-29 21:55:40 +0300

    fix cli

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,7 +2,7 @@ import prompt
 
 
 def welcome_user():
-    print('Welcom to the brain Games!')
+    print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     return name
